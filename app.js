4d0aa036364e51080f58e496b903279b8018621c
Russell Chreptyk <russell@chreptyk.com>
2014-11-25 14:59:23 -0500

    Added more CORS stuff

--- app.js
+++ app.js
@@ -27,6 +27,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(function(req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
 });
 
