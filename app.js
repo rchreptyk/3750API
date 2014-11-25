3544bbeffc9d427dc8c40ffbac48e18233aae675
Russell Chreptyk <russell@chreptyk.com>
2014-11-25 15:00:54 -0500

    Handling options

--- app.js
+++ app.js
@@ -29,7 +29,13 @@ app.use(function(req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
-    next();
+    
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
 });
 
 app.use(auth.authenticator);
